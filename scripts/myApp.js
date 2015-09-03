ec31c3045b6ca63b627aef51824b24d7528b2afd
John Harden <jharden@pnimedia.com>
2015-09-03 12:58:47 -0700

    initial commit

---
+++ scripts/myApp.js
@@ -0,0 +1,68 @@
+/**
+ * Created by jharden on 9/2/2015.
+ */
+
+//declaration of the angular module
+var myApp = angular.module('myApp',[])
+
+//controller declaration - controller handles back end functions and binds them to the 'scope'
+.controller('myAppController',function($scope){
+
+    //controller will contain backend functions and variables, but we can set our own variables there too when nessecary:
+    //a boolean to tell us whether we are in mobile view or not
+    $scope.isMobile = false;
+
+    $scope.helloWorld = "Hello World!";
+
+//we can bind the responsive directive to any element within the scope of a page. declare it by adding it after the controller declaration
+}).directive('responsive',function($window){
+
+       return{
+
+           link: function(scope){
+
+               //inside the link function you can add any functions you want, including resize and scroll events
+
+               var smallWidth  =  768;
+
+               var setSmall = function () {
+                   console.log('mobile');
+
+                   //in a directive if we want to change a variable that belongs to the controller you need to force the scope to update using the following:
+                   scope.$apply(function(){
+                       scope.isMobile = true;
+                   })
+               };
+
+               var setLarge = function () {
+                   console.log('desktop');
+                   scope.$apply(function(){
+                       scope.isMobile = false;
+                   })
+               };
+
+               //window events can be created here:
+               //window.resize
+               angular.element($window).on('resize',function(){
+                   if ($window.innerWidth <= smallWidth) {
+                       setSmall();
+                   } else {
+                       setLarge();
+                   }
+               });
+               //window.scroll;
+               angular.element($window).on('scroll',function(){
+                  console.log('activated scroll');
+               });
+
+
+           }
+
+       }
+
+
+
+
+
+
+});
